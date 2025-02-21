a00ed10581947065a9cd8bfebea773f8b352bac6
kwang <shinkwang@yonsei.ac.kr>
2025-02-21 12:21:40 +0900

    refactor: basic redis

--- basic/redis/src/test/java/shinstyle/redis/service/CacheServiceTest.java
+++ basic/redis/src/test/java/shinstyle/redis/service/CacheServiceTest.java
@@ -1,6 +1,5 @@
 package shinstyle.redis.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +8,7 @@ import shinstyle.redis.model.User;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CacheServiceTest {
