951eebd5d8852b280cbb774c9f6bb9e2face2771
kwang <shinkwang@yonsei.ac.kr>
2025-03-03 14:02:11 +0900

    test: Coupon issue concurrency

---
+++ promotion/coupon-service/src/test/java/shinstyle/couponservice/service/v1/CouponServiceConcurrencyTest.java
@@ -0,0 +1,62 @@
+package shinstyle.couponservice.service.v1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import shinstyle.couponservice.domain.CouponPolicy;
+import shinstyle.couponservice.dto.v1.CouponDto;
+import shinstyle.couponservice.exception.CouponIssueException;
+import shinstyle.couponservice.repository.CouponPolicyRepository;
+import shinstyle.couponservice.repository.CouponRepository;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.*;
+import static shinstyle.couponservice.service.v1.CouponServiceTest.givenCouponPolicy;
+
+@SpringBootTest(properties = "eureka.client.enabled=false")
+public class CouponServiceConcurrencyTest {
+
+    @Autowired
+    CouponService couponService;
+
+    @Autowired
+    CouponRepository couponRepository;
+
+    @Autowired
+    CouponPolicyRepository couponPolicyRepository;
+
+    @Test
+    void 여러명_쿠폰_신청() throws Exception {
+        // Given
+        CouponPolicy couponPolicy = givenCouponPolicy();
+        CouponPolicy savedCouponPolicy = couponPolicyRepository.save(couponPolicy);
+
+        int threadCount = 300;
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        for (long i = 0; i < threadCount; i++) {
+            Long userId = Long.valueOf(i);
+            executorService.execute(() -> {
+                try {
+                    couponService.issueCoupon(CouponDto.IssueRequest.builder()
+                            .couponPolicyId(savedCouponPolicy.getId())
+                            .build(), userId);
+                } catch (CouponIssueException e) {
+                    System.out.println("쿠폰 발급 실패: " + userId);
+                } finally {
+                    latch.countDown();
+                }
+
+            });
+        }
+        latch.await();
+
+        assertThat(couponRepository.countByCouponPolicyId(savedCouponPolicy.getId())).isEqualTo(100);
+    }
+}
+
